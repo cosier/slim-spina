b240c9880441cf7ed22b93d0d8f219902f3ada5e
Bailey Cosier <bailey.cosier@gmail.com>
2021-11-24 03:52:46 +0700

    Add support for icons within navigation sidebar

--- app/components/spina/main_navigation/link_component.rb
+++ app/components/spina/main_navigation/link_component.rb
@@ -1,11 +1,17 @@
 module Spina
   module MainNavigation
     class LinkComponent < ApplicationComponent
+      include Spina::Admin::IconsHelper
 
-      def initialize(label, path, active: false)
+      def initialize(label, path, active: false, icon: nil)
         @label = label
         @path = path
         @active = active
+        @icon = icon && icon(icon)
+      end
+
+      def icon(icon_name)
+        heroicon(icon_name, style: :solid, class: "inline-block w-8 h-8 text-white md:mr-3")
       end
 
       def css_classes
--- app/helpers/spina/admin/icons_helper.rb
+++ app/helpers/spina/admin/icons_helper.rb
@@ -2,10 +2,10 @@ require 'nokogiri'
 
 module Spina::Admin
 	module IconsHelper
-	
+
 		class FileNotFound < StandardError
 	  end
-		
+
 		def heroicon(name, style: :outline, **options)
 	    file = read_file(Spina::Engine.root.join("app/assets/icons/heroicons", style.to_s, "#{name}.svg"))
 	    return "" if file.nil?
@@ -14,13 +14,13 @@ module Spina::Admin
 	    svg[:class] = options[:class]
 	    ActiveSupport::SafeBuffer.new(svg.to_s)
 		end
-		
+
 		private
-		
+
 	    def read_file(path)
 	      return nil unless File.exist?(path)
 	      File.read(path)
-	    end  
+	    end
 
 	end
 end
